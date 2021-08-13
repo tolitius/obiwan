d7bd238e4fc1942124b3665ebad81f081c3f854d
anatoly <tolitius@gmail.com>
2021-08-13 01:21:02 -0400

    welcome obiwan!

---
+++ README.md
@@ -0,0 +1,122 @@
+# obi wan
+
+> _"These aren't the droids you're looking for"_
+
+[![<! release](https://img.shields.io/badge/dynamic/json.svg?label=release&url=https%3A%2F%2Fclojars.org%2Fcom.tolitius%2Fobiwan%2Flatest-version.json&query=version&colorB=blue)](https://github.com/tolitius/obiwan/releases)
+[![<! clojars>](https://img.shields.io/clojars/v/com.tolitius/obiwan.svg)](https://clojars.org/com.tolitius/obiwan)
+
+redis clojure client based on [jedis](https://github.com/redis/jedis).
+
+- [spilling the beans](#spilling-the-beans)
+- [license](#license)
+
+## spilling the beans
+
+```clojure
+$ make repl
+
+=> (require '[obiwan.core :as redis])
+
+=> (def conn (redis/create-pool))
+#'user/conn
+
+=> (redis/pool-stats conn)
+{:active-resources 0, :number-of-waiters 0, :idle-resources 0}
+```
+
+hash
+
+```clojure
+=> (redis/hmset conn "solar-system" {"mercury"  "0.33 x 10^24 kg"
+                                     "venus"    "4.867 x 10^24 kg"
+                                     "earth"    "5.972 x 10^24 kg"
+                                     "mars"     "0.65 x 10^24 kg"
+                                     "jupiter"  "1900 x 10^24 kg"
+                                     "saturn"   "570 x 10^24 kg"
+                                     "uranus"   "87 x 10^24 kg"
+                                     "neptune"  "100 x 10^24 kg"
+                                     "pluto"    "1.3 × 10^22 kg"})
+"OK"
+
+=> (redis/hmget conn "solar-system" ["earth" "mars"])
+["5.972 x 10^24 kg" "0.65 x 10^24 kg"]
+
+=> (redis/hgetall conn "solar-system")
+{"earth" "5.972 x 10^24 kg",
+ "saturn" "570 x 10^24 kg",
+ "jupiter" "1900 x 10^24 kg",
+ "pluto" "1.3 × 10^22 kg",
+ "uranus" "87 x 10^24 kg",
+ "mercury" "0.33 x 10^24 kg",
+ "neptune" "100 x 10^24 kg",
+ "mars" "0.65 x 10^24 kg",
+ "venus" "4.867 x 10^24 kg"}
+```
+
+sorted set
+
+```clojure
+user=> (redis/zadd conn "planets" {"mercury" 1.0
+                                   "venus"   2.0
+                                   "earth"   3.0
+                                   "mars"    4.0
+                                   "jupiter" 5.0
+                                   "saturn"  6.0
+                                   "uranus"  7.0
+                                   "neptune" 8.0
+                                   "pluto"   9.0})
+9
+
+=> (redis/zrange conn "planets" 3 7)
+#{"mars" "jupiter" "saturn" "uranus" "neptune"}
+
+=> (redis/zrange conn "planets" 0 -1)
+#{"mercury" "venus" "earth" "mars" "jupiter" "saturn" "uranus" "neptune" "pluto"}
+```
+
+looking inside the source (redis server):
+
+```bash
+127.0.0.1:6379> keys *
+1) "planets"
+2) "solar-system"
+
+127.0.0.1:6379> hgetall "solar-system"
+ 1) "mars"
+ 2) "0.65 x 10^24 kg"
+ 3) "pluto"
+ 4) "1.3 \xc3\x97 10^22 kg"
+ 5) "jupiter"
+ 6) "1900 x 10^24 kg"
+ 7) "uranus"
+ 8) "87 x 10^24 kg"
+ 9) "earth"
+10) "5.972 x 10^24 kg"
+11) "mercury"
+12) "0.33 x 10^24 kg"
+13) "venus"
+14) "4.867 x 10^24 kg"
+15) "saturn"
+16) "570 x 10^24 kg"
+17) "neptune"
+18) "100 x 10^24 kg"
+
+127.0.0.1:6379> zrange planets 0 -1
+1) "mercury"
+2) "venus"
+3) "earth"
+4) "mars"
+5) "jupiter"
+6) "saturn"
+7) "uranus"
+8) "neptune"
+9) "pluto"
+127.0.0.1:6379>
+```
+
+## license
+
+Copyright © 2021 tolitius
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/obiwan/core.clj
@@ -0,0 +1,123 @@
+(ns obiwan.core
+  (:require [clojure.tools.logging :as log])
+  (:import [redis.clients.jedis Jedis
+                                Protocol
+                                JedisPool
+                                JedisPoolConfig
+                                ScanParams
+                                ScanResult]
+           [org.apache.commons.pool2.impl GenericObjectPool]))
+
+(defn new-conn [^JedisPool pool]
+  (.getResource pool))
+
+(defn op [pool f]
+  (with-open [^Jedis r (new-conn pool)]
+    (f r)))
+
+(defn create-pool
+  ([]
+   (create-pool {}))
+  ([{:keys [host port pool timeout password]
+     :or {host "127.0.0.1"
+          port 6379
+          timeout Protocol/DEFAULT_TIMEOUT
+          pool {:size 42
+                :max-wait 30000}}}]
+   (let [conf (doto (JedisPoolConfig.)
+                (.setMaxTotal (pool :size))
+                (.setMaxWaitMillis (pool :max-wait)))]
+     (log/info (str "connecting to Redis " host ":" port ", timeout: " timeout ", pool: " pool))
+     (JedisPool. conf ^String host ^int port ^int timeout ^String password))))
+
+(defn close-pool [pool]
+  (log/info "disconnecting from Redis:" pool)
+  (.destroy pool))
+
+(defn pool-stats [pool]
+  {:active-resources (.getNumActive pool)
+   ; :max-total (.getMaxTotal pool)
+   ; :max-wait-ms (.getMaxWaitMillis pool)
+   ; :created-count (.getCreatedCount pool)
+   ; :returned-count (.getReturnedCount pool)
+   :number-of-waiters (.getNumWaiters pool)
+   :idle-resources (.getNumIdle pool)})
+
+;; wrap Java methods to make them composable
+
+;; hash
+
+(defn hget [^JedisPool redis h f]
+  (op redis #(.hget % h f)))
+
+(defn hmget [^JedisPool redis h fs]
+  (into [] (op redis #(.hmget % h (into-array String fs)))))
+
+(defn hgetall [^JedisPool redis h]
+  (into {} (op redis #(.hgetAll % h))))
+
+(defn hset [redis h f v]
+  (op redis #(.hset % h f v)))
+
+(defn hmset [redis h m]
+  (op redis #(.hmset % h m)))
+
+(defn hdel [redis h & v]
+  (op redis #(.hdel % h (into-array String v))))
+
+;; sorted set
+
+(defn zadd [redis s m]
+  (op redis #(.zadd % s m)))
+
+(defn zrange [redis s zmin zmax]
+  (op redis #(.zrange % s zmin zmax)))
+
+;; set
+
+(defn smembers [redis s]
+  (op redis #(.smembers % s)))
+
+(defn scard [redis s]
+  (op redis #(.scard % s)))
+
+(defn sismember [redis s v]
+  (op redis #(.sismember % s v)))
+
+(defn sadd [redis s v]
+  (op redis #(.sadd % s (into-array
+                          String [v]))))
+
+(defn srem [redis s v]
+  (op redis #(.srem % s (into-array
+                         String [v]))))
+
+;; scaning things
+
+(defn new-scan-params [{:keys [fetch-size pattern]
+                        :or {fetch-size 10}}]
+  (doto (ScanParams.)
+        (.count (int fetch-size))))
+
+(defn sscan
+  ([redis s cur]
+   (sscan redis s cur {}))
+  ([redis s cur params]
+   (let [^ScanResult rs (op redis #(.sscan % s cur (new-scan-params params)))
+         batch  (.getResult rs)
+         cursor (.getCursor rs)]
+     {:batch batch :cursor cursor :done? (.isCompleteIteration rs)})))
+
+
+;; TODO: if/when needed generalize to scan, hscan, zscan
+(defn scan-all
+  "scan a whole set 's' and apply a function 'f' on each 'batch'"
+  ([redis s f]
+   (scan-all redis s f {}))
+  ([redis s f params]
+   (loop [cursor "0"
+          rs (sscan redis s "0" params)]
+     (let [{:keys [batch cursor done?]} rs]
+       (f batch)
+       (when-not done?
+         (recur cursor (sscan redis s cursor params)))))))
