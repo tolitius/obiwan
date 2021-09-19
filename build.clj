0959f338370a72b81baea88c5fde314b1edcd86a
anatoly <tolitius@gmail.com>
2021-09-18 21:08:38 -0400

    up to 0.1.477

--- build.clj
+++ build.clj
@@ -8,7 +8,7 @@
 
 (defonce project
   (let [lib        'com.tolitius/obiwan
-        version    "0.1.476"
+        version    "0.1.477"
         target-dir "target"]
     {:lib lib
      :description "redis clojure client based on jedis"
